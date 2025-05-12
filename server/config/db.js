f011c42e58eaffe90a44b8abd26234329f8a3263
Shivani Kumari <pandeyshivani2112@gmail.com>
2025-05-13 00:38:21 +0530

    mongodb uri updated

--- server/config/db.js
+++ server/config/db.js
@@ -2,13 +2,14 @@ import mongoose from "mongoose";
 
 // Function to connect to the MongoDB database
 const connectDB = async () => {
+    try {
+        mongoose.connection.on('connected', () => console.log('Database Connected'))
 
-    mongoose.connection.on('connected', () => console.log('Database Connected'))
+        await mongoose.connect(`${process.env.MONGODB_URI}/job-portal`)
+    } catch (error) {
+        console.log('DB connection Failed!: ', error)
+    }
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/job-portal`,{
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
 }
 
 export default connectDB
